b055f6f49ff04a81b71eb5899af844e408fb2a3a
lager <machv@cesnet.cz>
2019-01-22 12:57:10 +0100

    use https and hostname

--- debug.sh
+++ debug.sh
@@ -10,7 +10,7 @@ i=0
 while [[ $curr -lt $lines ]]
 do
   data=$(sed -n $curr,$((curr + 71))p $1 | sed '1 s/\]\[/\[/' | jq -c '.')
-  echo "$data" | curl http://195.113.233.246:8088/data -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' --data-binary @- # $'$data'
+  echo "$data" | curl https://monitor.eduroam.cz:8088/data -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' --data-binary @- # $'$data'
   curr=$((curr + 71))
   ((i++))
 
